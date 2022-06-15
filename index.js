9d2e031da03ce545e58957468e2accfb47d0ec9b
Nico Picchio <nicolapicchio@gmail.com>
2022-06-15 16:09:03 +0100

    commit for deployment

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 import cors from 'cors';
 
 const app = express();
-const url = process.env.URL
+const apiUrl = process.env.API_URL
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  axios.get(`${url}${req.userUrl}`)
+app.use((req, res) => {
+  axios.get(`${apiUrl}${req.userUrl}`)
   .then(response => res.json(response.data))
   .catch(e => console.error("err", e.message))
 })
