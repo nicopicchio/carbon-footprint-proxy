bc4da701f55c2f09f95b347c103a4c5a4da70e77
Nico Picchio <nicolapicchio@gmail.com>
2022-06-25 20:42:27 +0100

    added json response for catch

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 require('dotenv').config();
-const express = require('express')
-const axios = require('axios')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3001
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
 
-app.use(cors())
+app.use(cors());
 
 app.use((req, res) => {
-    const reqUrl = req.originalUrl.replace('/', '')
-    const url = `${process.env.API_URL}${reqUrl}`
-    axios.get(url)
-    .then(response => {
-        res.json(response.data)
-    })
-    .catch(e => console.error("err", e.message))
-})
+	const reqUrl = req.originalUrl.replace('/', '');
+	const url = `${process.env.API_URL}${reqUrl}`;
+	axios
+		.get(url)
+		.then((response) => {
+			res.json(response.data);
+		})
+		.catch((e) => {
+			res.status(500).json({ error: 'Error calling the API' });
+			console.error('err', e.message);
+		});
+});
 
 app.listen(port, () => {
-    console.log('Listening on', port)
-})
+	console.log('Listening on', port);
+});
